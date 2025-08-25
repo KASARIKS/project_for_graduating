73fd5c80f1b437a9de5475fe84a877e45f596e71
Golovko, Yaroslav <golovko085@gmail.com>
2025-08-25 20:08:02 +0700

    first task

---
+++ internal/server/server.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type routerData struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+func CreateServer(logger *log.Logger) (*routerData, error) {
+	registerHandlers()
+	routerData := newRouterData(logger)
+	return routerData, nil
+}
+
+func registerHandlers() {
+	http.Handle("/", http.FileServer(http.Dir("web")))
+	// http.HandleFunc("/", handlers.Main)
+	// http.HandleFunc("/js/scripts.min.js", handlers.ScriptMinJs)
+	// http.HandleFunc("/css/style.css", handlers.StyleCss)
+	// http.HandleFunc("/favicon.ioc", handlers.FaviconIco)
+}
+
+func newRouterData(logger *log.Logger) *routerData {
+	todoPort := os.Getenv("TODO_PORT")
+	if todoPort == "" {
+		todoPort = ":7540"
+	}
+
+	server := &http.Server{
+		Addr:         todoPort,
+		Handler:      http.DefaultServeMux,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		IdleTimeout:  time.Second * 5,
+	}
+
+	router := &routerData{
+		Logger: logger,
+		Server: server,
+	}
+
+	return router
+}
