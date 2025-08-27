6de85c3988e66ae9c33fc79f44d48970f16fb5e9
Golovko, Yaroslav <golovko085@gmail.com>
2025-08-27 19:02:46 +0700

    basic refactoring of validate.go

--- internal/nextdate/validate.go
+++ internal/nextdate/validate.go
@@ -15,9 +15,6 @@ func validateRepeatParam(repeat string) error {
 	}
 
 	option := splitedRepeat[0]
-	if option == "m" {
-
-	}
 
 	if !slices.Contains(firstLetters, option) {
 		return errors.New("unsupported option")
@@ -33,66 +30,90 @@ func validateRepeatParam(repeat string) error {
 func validateRepeatbyOption(option string, repeat string) error {
 	switch option {
 	case "d":
-		splitedRepeat := splitRepeat(repeat)
-		if len(splitedRepeat) != 2 {
-			return errors.New("unsupported format length for d option")
-		}
-		n, err := strconv.Atoi(splitedRepeat[1])
-		if err != nil {
-			return errors.New("not numbers in repeat for d option")
-		}
-		if n > 400 {
-			return errors.New("too big number for d option")
-		}
+		return validateRepeatD(repeat)
 	case "y":
-		splitedRepeat := splitRepeat(repeat)
-		if len(splitedRepeat) > 1 {
-			return errors.New("unsupported format length for y option")
-		}
+		return validateRepeatY(repeat)
 	case "w":
-		splitedRepeat := splitRepeat(repeat)
-		if len(splitedRepeat) < 2 {
-			return errors.New("unsupported format length for w option")
-		}
-		// Check for numbers
-		for _, v := range splitedRepeat[1:] {
-			n, err := strconv.Atoi(v)
-			if err != nil {
-				return errors.New("not numbers in repeat for w option")
-			}
-			if n > 7 || n < 1 {
-				return errors.New("incorrect numbers in repeat for w option")
-			}
-		}
+		return validateRepeatW(repeat)
 	case "m":
-		splitedRepeatOnlySpaces := strings.Split(repeat, " ")
-		if len(splitedRepeatOnlySpaces) < 2 {
-			return errors.New("unsupported format length for m option")
-		}
+		return validateRepeatM(repeat)
+	default:
+		return errors.New("unsupported option")
+	}
+}
 
-		// Check for days numbers
-		days := strings.Split(splitedRepeatOnlySpaces[1], ",")
-		for _, v := range days {
-			day, err := strconv.Atoi(v)
+func validateRepeatD(repeat string) error {
+	splitedRepeat := splitRepeat(repeat)
+	if len(splitedRepeat) != 2 {
+		return errors.New("unsupported format length for d option")
+	}
+	n, err := strconv.Atoi(splitedRepeat[1])
+	if err != nil {
+		return errors.New("not numbers in repeat for d option")
+	}
+	if n > 400 {
+		return errors.New("too big number for d option")
+	}
+
+	return nil
+}
+
+func validateRepeatY(repeat string) error {
+	splitedRepeat := splitRepeat(repeat)
+	if len(splitedRepeat) > 1 {
+		return errors.New("unsupported format length for y option")
+	}
+
+	return nil
+}
+
+func validateRepeatW(repeat string) error {
+	splitedRepeat := splitRepeat(repeat)
+	if len(splitedRepeat) < 2 {
+		return errors.New("unsupported format length for w option")
+	}
+	// Check for numbers
+	for _, v := range splitedRepeat[1:] {
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			return errors.New("not numbers in repeat for w option")
+		}
+		if n > 7 || n < 1 {
+			return errors.New("incorrect numbers in repeat for w option")
+		}
+	}
+
+	return nil
+}
+
+func validateRepeatM(repeat string) error {
+	splitedRepeatOnlySpaces := strings.Split(repeat, " ")
+	if len(splitedRepeatOnlySpaces) < 2 {
+		return errors.New("unsupported format length for m option")
+	}
+
+	// Check for days numbers
+	days := strings.Split(splitedRepeatOnlySpaces[1], ",")
+	for _, v := range days {
+		day, err := strconv.Atoi(v)
+		if err != nil {
+			return errors.New("not numbers in days in repeat for m option")
+		}
+		if day > 31 || day < -31 {
+			return errors.New("too big or too small numbers in days in repeat for m option")
+		}
+	}
+
+	// Check for months numbers
+	if len(splitedRepeatOnlySpaces) > 2 {
+		months := strings.Split(splitedRepeatOnlySpaces[2], ",")
+		for _, v := range months {
+			month, err := strconv.Atoi(v)
 			if err != nil {
-				return errors.New("not numbers in days in repeat for m option")
+				return errors.New("not numbers in months in repeat for m option")
 			}
-			if day > 31 || day < -31 {
-				return errors.New("too big or too small numbers in days in repeat for m option")
-			}
-		}
-
-		// Check for months numbers
-		if len(splitedRepeatOnlySpaces) > 2 {
-			months := strings.Split(splitedRepeatOnlySpaces[2], ",")
-			for _, v := range months {
-				month, err := strconv.Atoi(v)
-				if err != nil {
-					return errors.New("not numbers in months in repeat for m option")
-				}
-				if month > 12 || month < 1 {
-					return errors.New("too big or too small numbers in months in repeat for m option")
-				}
+			if month > 12 || month < 1 {
+				return errors.New("too big or too small numbers in months in repeat for m option")
 			}
 		}
 	}
