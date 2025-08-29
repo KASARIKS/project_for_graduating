d5c0e3ef5d97f47debae8a038a61dea50736ec22
Golovko, Yaroslav <golovko085@gmail.com>
2025-08-29 17:17:53 +0700

    basic task handler

--- internal/db/dbEntites/dbTask/dbTask.go
+++ internal/db/dbEntites/dbTask/dbTask.go
@@ -1,4 +1,32 @@
 package dbtask
 
 type DbTask struct {
+	Id      int    `json:"id"`
+	Date    string `json:"date"`
+	Title   string `json:"title"`
+	Comment string `json:"comment"`
+	Repeat  string `json:"repeat"`
+}
+
+func NewDbTask(id int, date, title, comment, repeat string) *DbTask {
+	newDbTask := &DbTask{
+		Id:      id,
+		Date:    date,
+		Title:   title,
+		Comment: comment,
+		Repeat:  repeat,
+	}
+
+	return newDbTask
+}
+
+func NewDbTaskWithoutId(date, title, comment, repeat string) *DbTask {
+	newDbTask := &DbTask{
+		Date:    date,
+		Title:   title,
+		Comment: comment,
+		Repeat:  repeat,
+	}
+
+	return newDbTask
 }
--- internal/handlers/task.go
+++ internal/handlers/task.go
@@ -1,13 +1,71 @@
 package handlers
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
+	"time"
+
+	"github.com/kasariks/project_for_graduating/internal/db"
+	dbtask "github.com/kasariks/project_for_graduating/internal/db/dbEntites/dbTask"
+	"github.com/kasariks/project_for_graduating/internal/nextdate"
 )
 
-func AddNewTask(w http.ResponseWriter, r *http.Request) {
+func Task(w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("Content-Type") != "application/json" {
 		http.Error(w, errors.New("wrong request format").Error(), http.StatusBadRequest)
+		return
 	}
 
+	switch r.Method {
+	case http.MethodPost:
+		var task dbtask.DbTask
+		if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if task.Title == "" {
+			http.Error(w, errors.New("empty title").Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Validate task date
+		now := time.Now()
+		if task.Date == "" {
+			task.Date = now.Format(nextdate.DateFormat)
+		}
+		t, err := time.Parse("20060102", task.Date)
+		if err != nil {
+			http.Error(w, errors.New("incorrect date").Error(), http.StatusBadRequest)
+			return
+		}
+		if task.Repeat != "" {
+			next, err := nextdate.NextDate(now, task.Date, task.Repeat)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			if now.After(t) {
+				if len(task.Repeat) == 0 {
+					task.Date = now.Format(nextdate.DateFormat)
+				} else {
+					task.Date = next
+				}
+			}
+		}
+
+		id, err := db.AddTask(&task)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Send id into repsonse in json
+		if err := json.NewEncoder(w).Encode(id); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 }
