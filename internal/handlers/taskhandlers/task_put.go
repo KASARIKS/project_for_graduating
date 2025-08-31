bd3e77db610f56f172ae0f39a3f3c06cf0632da0
Golovko, Yaroslav <golovko085@gmail.com>
2025-09-01 00:33:24 +0700

    task put partially

--- internal/handlers/taskhandlers/task_put.go
+++ internal/handlers/taskhandlers/task_put.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/kasariks/project_for_graduating/internal/db"
 	dbtask "github.com/kasariks/project_for_graduating/internal/db/dbEntites/dbTask"
 )
 
 func taskPut(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	var dbTask dbtask.DbTask
 
 	body, err := io.ReadAll(r.Body)
@@ -18,11 +20,24 @@ func taskPut(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = json.Unmarshal(body, &dbTask)
+	var taskMap map[string]string
+
+	err = json.Unmarshal(body, &taskMap)
 	if err != nil {
 		writeErrorInJson(w, err)
 		return
 	}
+
+	id, err := strconv.Atoi(taskMap["id"])
+	if err != nil {
+		writeErrorInJson(w, err)
+		return
+	}
+	dbTask.Id = id
+	dbTask.Date = taskMap["date"]
+	dbTask.Title = taskMap["title"]
+	dbTask.Comment = taskMap["comment"]
+	dbTask.Repeat = taskMap["repeat"]
 
 	err = db.UpdateTask(&dbTask)
 	if err != nil {
